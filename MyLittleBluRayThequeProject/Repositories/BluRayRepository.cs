4e31f3e5e606db10d31b1b8d1e166b91cd6a7614
Romain RABOUAN <romain.rabouan@live.fr>
2021-12-07 14:57:43 +0100

    Ajout liste (statique) de blu-rays

--- MyLittleBluRayThequeProject/Repositories/BluRayRepository.cs
+++ MyLittleBluRayThequeProject/Repositories/BluRayRepository.cs
@@ -22,6 +22,24 @@ namespace MyLittleBluRayThequeProject.Repositories
                     Titre = "My Little film 1",
                     DateSortie = DateTime.Now,
                     Version = "Courte",
+                    Scenariste = new Personne
+                    {
+                        Id = 1000,
+                        Nom = "Nom Scenar 1",
+                        Prenom = "Prenom Scenar 1",
+                        Nationalite = "Fr",
+                        DateNaissance = DateTime.Now,
+                        Professions = new List<string>{"Scénariste"}
+                    },
+                    Realisateur = new Personne
+                    {
+                        Id = 1001,
+                        Nom = "Nom Réal 1",
+                        Prenom = "Prenom Réal 1",
+                        Nationalite = "Fr",
+                        DateNaissance = DateTime.Now,
+                        Professions = new List<string>{"Réalisateur"}
+                    },
                     Acteurs = new List<Personne>
                     {
                         new Personne
@@ -41,6 +59,24 @@ namespace MyLittleBluRayThequeProject.Repositories
                     Titre = "My Little film 2",
                     DateSortie = DateTime.Now,
                     Version = "Longue",
+                    Scenariste = new Personne
+                    {
+                        Id = 1002,
+                        Nom = "Nom Scenar 2",
+                        Prenom = "Prenom Scenar 2",
+                        Nationalite = "Fr",
+                        DateNaissance = DateTime.Now,
+                        Professions = new List<string>{"Scénariste"}
+                    },
+                    Realisateur = new Personne
+                    {
+                        Id = 1003,
+                        Nom = "Nom Réal 2",
+                        Prenom = "Prenom Réal 2",
+                        Nationalite = "Fr",
+                        DateNaissance = DateTime.Now,
+                        Professions = new List<string>{"Réalisateur"}
+                    },
                     Acteurs = new List<Personne>
                     {
                         new Personne
